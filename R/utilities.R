28e61d96f2d3fcc53c56fbc1e6688d1d0d6c11f9
Lucas Friesen <lucas.j.friesen@gmail.com>
2019-05-09 18:55:20 -0700

    .

---
+++ R/utilities.R
@@ -0,0 +1,32 @@
+
+
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;border-width:1px;border-style:solid;border-color:black;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:0px;overflow:hidden;word-break:normal;}
+.tg .tg-s6z2{text-align:center}
+.tg .tg-uys7{border-color:inherit;text-align:center}
+.tg .tg-h0x1{text-align:center}
+</style>
+<table class="tg">
+  <tr>
+    <td class="tg-s6z2"></td>
+    <td class="tg-uys7" colspan="3">DECISION BASED ON MEASURE</td>
+  </tr>
+  <tr>
+    <td class="tg-h0x1" rowspan="3">CRITERION</td>
+    <td class="tg-h0x1"></td>
+    <td class="tg-h0x1">Negative</td>
+    <td class="tg-h0x1">Positive</td>
+  </tr>
+  <tr>
+    <td class="tg-s6z2">Negative</td>
+    <td class="tg-s6z2">X (TN)</td>
+    <td class="tg-s6z2">X (FP)</td>
+  </tr>
+  <tr>
+    <td class="tg-h0x1">Positive</td>
+    <td class="tg-h0x1">X (FN)</td>
+    <td class="tg-h0x1">X (TP)</td>
+  </tr>
+</table>
